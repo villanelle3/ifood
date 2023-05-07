20da39b87d7d7540bca1bae7001eb6a2a35f447c
Laura Augusta <laurah.algusta@gmail.com>
2023-05-06 23:01:22 -0300

    API integration

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -19,11 +19,34 @@ const PaginaInicial = function PaginaInicial(){
         }
     }, []);
     const Restaurantes = [
-        new Restaurante(0, 'Antonieta Ristorante', 5.0, 'Italiana', true, 'https://media.gazetadopovo.com.br/bomgourmet/2016/05/parmegiana-cae978d0.jpg'),
-        new Restaurante(1, 'Bio Bistrot', 4.5, 'Vegana', false, 'https://www.acasaencantada.com.br/wp-content/uploads/2021/03/Comida-vegana-alface-grao-de-bico-tomatinhos-cereja-abacate-e-folhas-verdes-1.webp'),
-        new Restaurante(2, 'Regional Grill', 4.8, 'Brasileira', false, 'https://www.comidaereceitas.com.br/wp-content/uploads/2008/07/Picanha-inteira-na-grelha-para-churrasco-freepik-780x520.jpg'),
-        new Restaurante(3, 'Dom Temakeria', 4.9, 'Japonesa', false, 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEggcx1H_iT4vTBkXJLj-T__RUoLFLe3K-vsJ5ZG6nLq1ZDCHfrs8M40wHFSndP5Zhvcx4b6_QwukYkDUzE1nAlQlR8mmmgpRH1iFrO5IDJV0urT5z_hRWUU0Pi6o3979x2cdJQJ-7iPJ2WIDeOhU6egT8-IbkzLRsDxUlWqpFSJkR4Nrn8ZT7u3b1Mk/s2000/receita-de-sushi.jpg')
+        new Restaurante(0, 'Sweet Cupcake', 5.0, 'Doceria', true, 'https://www.sabornamesa.com.br/media/k2/items/cache/8f096abef1c84b031550e510893c2d4d_XL.jpg'),
     ]
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    useEffect(() => {
+        fetch(`https://my-json-server.typicode.com/villanelle3/restaurantAPI/restaurantes`)
+            .then((response) => {
+                if (!response.ok) 
+                {
+                throw new Error(`This is an HTTP error: The status is ${response.status}`);
+                }
+                return response.json();
+            })
+            .then((actualData) => {
+                console.log(actualData) // Print data
+                setData(actualData);
+                setError(null);
+            })
+            .catch((err) => {
+                console.log(err.message); // Print data
+                setError(err.message);
+                setData(null);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+        }, []);
     return(
         <MainPage>
             <Container>
@@ -45,10 +68,13 @@ const PaginaInicial = function PaginaInicial(){
                                         <SpanLeft>{item.nota} <span className='card__body__star'><i className="bi bi-star-fill"></i></span> </SpanLeft>
                                     </Card.Title>
                                     <Card.Text className='card__body__text'>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                                        Nisi harum sed, earum dolore officia amet blanditiis eum 
-                                        veniam suscipit maxime rem accusamus odio itaque sint, alias 
-                                        ullam deleniti consequuntur asperiores?
+                                    Our cupcakes are perfect for birthdays, weddings, baby showers, and any other special event 
+                                    that calls for a sweet celebration. We also offer custom designs and flavors to make your 
+                                    event truly unique. Whether you're in the mood for a sweet treat or need a special dessert 
+                                    for your next event, we invite you to come and experience the joy and delight of our cupcake 
+                                    store. We take pride in our warm and inviting atmosphere, where you can indulge in a decadent 
+                                    cupcake or a hot beverage with friends and family. Our friendly staff are always on hand to 
+                                    provide recommendations and help you select the perfect treat for any occasion.
                                     </Card.Text>
                                     <SaibaMaisContainer>
                                         <a href={`/restaurant/${item.id}`}><Tag children="Saiba mais" /></a>
@@ -57,6 +83,38 @@ const PaginaInicial = function PaginaInicial(){
                             </Card>
                         </Col>
                     ))}
+                    {loading && <div>A moment please...</div>}
+                    {error && (
+                        <div>{`There is a problem fetching the post data - ${error}`}</div>
+                    )}
+                    {data &&
+                    data.map(({ id, name, stars, bio, category, image, destaque }) => (
+                        <Col className="d-flex justify-content-center" key={id}>
+                            <Card style={{ width: `${ ScreenWidth <= 768 ? ' ' : '472px' }` }}>
+                                <Card.Img variant="top" src={image} alt={name} style={{position: "relative"}} />
+                                {destaque ? 
+                                    <SpanDestaque><Tag children="Destaque da semana" /></SpanDestaque> 
+                                    : "" 
+                                }
+                                <Span>
+                                    <Tag children={category} />
+                                </Span>
+                                <Card.Body className='card__body'>
+                                    <Card.Title className='card__body__title' style={{fontWeight: "bold"}}>
+                                        {name}
+                                        <SpanLeft>{stars} 
+                                            <span className='card__body__star'><i className="bi bi-star-fill"></i></span> 
+                                        </SpanLeft>
+                                    </Card.Title>
+                                    <Card.Text className='card__body__text'>{bio}</Card.Text>
+                                    <SaibaMaisContainer>
+                                        <a href={`/restaurant/${id}`}><Tag children="Saiba mais" /></a>
+                                    </SaibaMaisContainer>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))
+                    }
                 </Row>
             </Container>
         </MainPage>
